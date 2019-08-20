19ab0ecc06e0194acc4de1cf2eb680ca3a670ad5
kristina.stoyanova <miss.krisi@abv.bg>
2019-08-20 18:49:21 +0300

    need to fix maven

--- src/dicegame/interfaces/Calculable.java
+++
@@ -1,5 +0,0 @@
-package dicegame.interfaces;
-
-public interface Calculable {
-    int calculateCombination(int dieNumber);
-}
--- src/dicegame/utils/printerfactory/Printer.java
+++
@@ -1,9 +0,0 @@
-package dicegame.utils.printerfactory;
-
-import dicegame.elements.Player;
-
-import java.util.List;
-
-public interface Printer {
-    void printEndGamePlayerStats(List<Player> playerList);
-}
